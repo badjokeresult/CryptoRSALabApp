f7d5f42db8c76f7f09221c37fe4d347e38744835
badjokeresult <badjokeresult@gmail.com>
2023-12-14 17:54:37 +0300

    gui-done-button-duplicates

--- CryptoRSALabApp/MainWindow.xaml.cs
+++ CryptoRSALabApp/MainWindow.xaml.cs
@@ -16,9 +16,49 @@ namespace CryptoRSALabApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private KeyPair? _firstKeyPair;
+        private KeyPair? _secondKeyPair;
+
+        private readonly RSAKeyGenerator _keyGenerator;
+        private readonly RSATextEncryptor _textEncryptor;
+
         public MainWindow()
         {
             InitializeComponent();
+            _keyGenerator = new RSAKeyGenerator();
+            _textEncryptor = new RSATextEncryptor();
+        }
+
+        private void firstKeysGeneratingButton_Click(object sender, RoutedEventArgs e)
+        {
+            _firstKeyPair = _keyGenerator.GenerateKeyPair();
+
+            firstPublicKeyTextBox.Text = _firstKeyPair.PublicKey.ToString();
+            firstPrivateKeyTextBox.Text = _firstKeyPair.PrivateKey.ToString();
+
+            secondCompanionPublicKeyTextBox.Text = _firstKeyPair.PublicKey.ToString();
+        }
+
+        private void secondKeysGeneratingButton_Click(object sender, RoutedEventArgs e)
+        {
+            _secondKeyPair = _keyGenerator.GenerateKeyPair();
+
+            secondPublicKeyTextBox.Text = _secondKeyPair.PublicKey.ToString();
+            secondPrivateKeyTextBox.Text= _secondKeyPair.PrivateKey.ToString();
+
+            firstCompanionPublicKeyTextBox.Text = _secondKeyPair.PublicKey.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (firstSourceMessageTextBox.Text.Length > 0)
+            {
+            }
+        }
+
+        private void firstReceiveAndDecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
